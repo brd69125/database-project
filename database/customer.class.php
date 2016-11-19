2d2121365b72294a944e6502a02f9ea19efb1c0c
Brody <brody.bruns695@topper.wku.edu>
2016-11-19 13:35:45 -0600

    added customer table class

---
+++ database/customer.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ *  description
+ * 
+ *  @category database files
+ *  @author Brody Bruns <brody.bruns@hotmail.com>
+ *  @copyright (c) 2016, Brody Bruns
+ *  @version 1.0
+ * 
+ */
+
+/**
+ * Description of customer
+ *
+ * @author Brody
+ */
+class Customer extends Database{
+    public $fields = ["id","name","address","phone","type","email"];
+    
+
+    
+}
