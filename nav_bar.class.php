5706d6c8ab2c2f98a75bcb778bc8d2114adf1e35
Brody <brody.bruns695@topper.wku.edu>
2016-11-19 15:52:35 -0600

    added nav bar

---
+++ nav_bar.class.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ *  description
+ * 
+ *  @category resource files
+ *  @author Brody Bruns <brody.bruns@hotmail.com>
+ *  @copyright (c) 2016, Brody Bruns
+ *  @version 1.0
+ * 
+ */
+
+/**
+ * Description of nav_bar
+ *
+ * @author Brody
+ */
+class Nav_Bar {
+    
+    protected $selected_page = "home";
+    protected $tab_key = "tab";
+    protected static $tab_list = ['home' => 'Home','sales'=>'Sales','services'=>'Services'];
+    
+    public function __construct() {
+        if(isset($_GET[$this->tab_key])){
+            $this->selected_page = $_GET[$this->tab_key];
+        }
+    }
+    
+    public function getDisplay(){
+        $display = "<form id='nav' class='nav_bar' method='get' action=''>";
+        foreach(self::$tab_list as $index=>$title){
+            $display .= self::getNavButton($index, $title) . "<br>";
+        }
+        $display .= "</form>";
+        return $display;
+    }
+    
+    public function displayCurrentPage(){
+        //choose tab
+        switch ($this->selected_page){
+            case "sales":
+                ;
+                break;
+            case "services":
+                ;
+                break;
+            default :
+                ;//home
+                break;
+        }
+    }
+    
+    public static function getNavButton($tab_name, $title){
+        $button = "<button type='submit' form='nav' name='$this->tab_key' value='$tab_name' class='nav_button'>$title</button>";
+        return $button;
+    }
+}
