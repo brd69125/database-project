2e7b78ec01f6e8f4b952c5ef818cbe80ab8f2de3
Brody <brody.bruns695@topper.wku.edu>
2016-11-20 17:55:14 -0600

    added customer display

--- database/customer.class.php
+++ database/customer.class.php
@@ -18,6 +18,16 @@
 class Customer extends Database{
     protected $fields = ["id","name","address","phone","type","email"];
     protected $tableName = "customer";
-
+    
+    public function getDisplay(){
+        $customer = "<div>";
+        $customer .= "Name: {$this->name}<br>";
+        $customer .= "Address: {$this->address}<br>";
+        $customer .= "Phone: {$this->phone}<br>";
+        $customer .= "Email: {$this->email}<br>";
+        $customer .= "Type: {$this->type}<br>";
+        $customer .= "</div>";
+        return $customer;
+    }
     
 }
