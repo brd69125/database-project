8d2badc09aebf7205a14fd3a279fbb515280b7de
Brody <brody.bruns695@topper.wku.edu>
2016-11-20 18:21:54 -0600

    updated sql

--- dealership.sql
+++ dealership.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2016 at 10:28 PM
+-- Generation Time: Nov 21, 2016 at 01:21 AM
 -- Server version: 5.6.23-log
 -- PHP Version: 5.6.12
 
@@ -28,12 +28,25 @@ USE `dealership`;
 -- Table structure for table `bill`
 --
 
+DROP TABLE IF EXISTS `bill`;
 CREATE TABLE IF NOT EXISTS `bill` (
   `id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `amount` int(11) NOT NULL,
   `payment` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `bill`
+--
+
+TRUNCATE TABLE `bill`;
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `date`, `amount`, `payment`) VALUES
+(1, '2016-11-20 22:51:57', 20, 'visa');
 
 -- --------------------------------------------------------
 
@@ -41,6 +54,7 @@ CREATE TABLE IF NOT EXISTS `bill` (
 -- Table structure for table `customer`
 --
 
+DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -48,7 +62,19 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `phone` int(11) DEFAULT NULL,
   `type` varchar(255) NOT NULL,
   `email` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `customer`
+--
+
+TRUNCATE TABLE `customer`;
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `name`, `address`, `phone`, `type`, `email`) VALUES
+(1, 'Chisom Ogbanana', '123 Place', 123456789, 'sale', 'nobody@nowhere.com');
 
 -- --------------------------------------------------------
 
@@ -56,6 +82,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 -- Table structure for table `employee`
 --
 
+DROP TABLE IF EXISTS `employee`;
 CREATE TABLE IF NOT EXISTS `employee` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -65,12 +92,18 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `type` varchar(255) NOT NULL COMMENT 'mechanic or sales'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `employee`
+--
+
+TRUNCATE TABLE `employee`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `sale`
 --
 
+DROP TABLE IF EXISTS `sale`;
 CREATE TABLE IF NOT EXISTS `sale` (
   `id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -78,7 +111,19 @@ CREATE TABLE IF NOT EXISTS `sale` (
   `customer` int(11) NOT NULL,
   `vehicle` int(11) NOT NULL,
   `bill` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `sale`
+--
+
+TRUNCATE TABLE `sale`;
+--
+-- Dumping data for table `sale`
+--
+
+INSERT INTO `sale` (`id`, `date`, `custom_work`, `customer`, `vehicle`, `bill`) VALUES
+(1, '2016-11-20 22:54:01', 'none', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -86,6 +131,7 @@ CREATE TABLE IF NOT EXISTS `sale` (
 -- Table structure for table `service_ticket`
 --
 
+DROP TABLE IF EXISTS `service_ticket`;
 CREATE TABLE IF NOT EXISTS `service_ticket` (
   `id` int(11) NOT NULL,
   `pickup_date` date NOT NULL,
@@ -101,19 +147,37 @@ CREATE TABLE IF NOT EXISTS `service_ticket` (
   `dep_mile` int(11) NOT NULL COMMENT 'departure mileage'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `service_ticket`
+--
+
+TRUNCATE TABLE `service_ticket`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `vehicle`
 --
 
+DROP TABLE IF EXISTS `vehicle`;
 CREATE TABLE IF NOT EXISTS `vehicle` (
   `id` int(11) NOT NULL,
   `make` varchar(255) NOT NULL,
   `model` varchar(255) NOT NULL,
   `year` int(11) NOT NULL,
   `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `vehicle`
+--
+
+TRUNCATE TABLE `vehicle`;
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`id`, `make`, `model`, `year`, `price`) VALUES
+(1, 'kia', 'soul', 1999, 10000);
 
 --
 -- Indexes for dumped tables
@@ -169,12 +233,12 @@ ALTER TABLE `vehicle`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `employee`
 --
@@ -184,7 +248,7 @@ ALTER TABLE `employee`
 -- AUTO_INCREMENT for table `sale`
 --
 ALTER TABLE `sale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `service_ticket`
 --
@@ -194,7 +258,7 @@ ALTER TABLE `service_ticket`
 -- AUTO_INCREMENT for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
